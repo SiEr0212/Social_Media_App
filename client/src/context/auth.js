cce850529c4fc72a60f6ea15f54f64d4b98955d0
SiEr0212 <simonertel@gmx.de>
2020-11-21 00:00:47 +0100

    adding login logout register to authcontext

---
+++ client/src/context/auth.js
@@ -0,0 +1,20 @@
+import React, { createContext } from 'react'
+
+const AuthContext = createContext({
+    user: null,
+    login: (data) => {},
+    logout: () => {}
+});
+
+function authReducer(state, action){
+    
+    switch(action.type){
+        case 'LOGIN' :
+            return{
+                ...state
+            }
+            default:
+                return state;
+    }
+}
+
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,42 +3,42 @@ import { Button, Form } from "semantic-ui-react";
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
-//import { useForm } from '../util/hooks';
+import { useForm } from "../util/hooks";
 
-function Register() {
-  const [values, setValues] = useState({
+function Register(props) {
+  const [errors, setErrors] = useState({});
+
+  const { onChange, onSubmit, values } = useForm(registerUser, {
     username: "",
     email: "",
     password: "",
     confirmPassword: "",
   });
 
-  const onChange = (event) => {
-    setValues({ ...values, [event.target.name]: event.target.value });
-  };
-
   const [addUser, { loading }] = useMutation(REGISTER_USER, {
     update(_, result) {
-      console.log(result);
+      props.history.push("/");
     },
-    variables: values
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    variables: values,
   });
 
-  const onSubmit =  (event) => {
-    event.preventDefault();
+  function registerUser() {
     addUser();
-  };
-
+  }
   return (
-    <div className='form-container'>
-      <Form onSubmit={onSubmit} noValidate>
+    <div className="form-container">
+      <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
         <h1>Register</h1>
         <Form.Input
           label="Username"
-          name="username" // thats the name that identifies it to use it later
           placeholder="Username.."
+          name="username" // thats the name that identifies it to use it later
           type="text"
           value={values.username}
+          error={errors.username ? true : false}
           onChange={onChange}
         />
         <Form.Input
@@ -47,6 +47,7 @@ function Register() {
           name="email" // thats the name that identifies it to use it later
           type="email"
           value={values.email}
+          error={errors.email ? true : false}
           onChange={onChange}
         />
         <Form.Input
@@ -55,6 +56,7 @@ function Register() {
           name="password" // thats the name that identifies it to use it later
           type="password"
           value={values.password}
+          error={errors.password ? true : false}
           onChange={onChange}
         />
         <Form.Input
@@ -63,12 +65,22 @@ function Register() {
           name="confirmPassword" // thats the name that identifies it to use it later
           type="password"
           value={values.confirmPassword}
+          error={errors.confirmPassword ? true : false}
           onChange={onChange}
         />
-      <Button className='register-btn' type="submit" primary>
-        Register
-      </Button>
+        <Button className="register-btn" type="submit" primary>
+          Register
+        </Button>
       </Form>
+      {Object.keys(errors).length > 0 && (
+        <div className="ui error message">
+          <ul className="list">
+            {Object.values(errors).map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
@@ -97,4 +109,4 @@ const REGISTER_USER = gql`
   }
 `;
 
-export default Register
+export default Register;
