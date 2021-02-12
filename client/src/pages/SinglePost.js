1e8bfc901b9215f5ba84ee0b0d72be5b9eb48640
SiEr0212 <simonertel@gmx.de>
2021-02-12 23:55:44 +0100

    added new component SinglePost

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,9 +1,49 @@
 import { SingleFieldSubscriptionsRule } from "graphql";
 import React from "react";
 import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
+import { Loader } from "semantic-ui-react";
 
 function SinglePost(props) {
   const postId = props.match.params.postid;
+  console.log(postId);
+  const {
+    data: { getPost },
+  } = useQuery(FETCH_POSTS_QUERY, {
+    variables: {
+      postId,
+    },
+  });
+
+  let postMarkup;
+  if (!getPost) {
+    postMarkup = (
+      <div class="ui segment">
+        <div class="ui active inverted dimmer">
+          <div class="ui text loader">Loading post...</div>
+        </div>
+        <p></p>
+      </div>
+    );
+  } else {
+    const {
+      id,
+      body,
+      createdAt,
+      username,
+      likes,
+      likeCount,
+      commentCount,
+    } = getPost;
+
+    postMarkup = {
+        <Grid>
+        
+        </Grid>
+    }
+
+  }
 }
 
 const FETCH_POST_QUERY = gql`
