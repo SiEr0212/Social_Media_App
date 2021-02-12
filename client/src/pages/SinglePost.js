33c3d2f2681402034be18f0169e064ee965cd6ce
SiEr0212 <simonertel@gmx.de>
2021-02-13 00:24:15 +0100

    modified  SinglePost

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,16 +1,16 @@
-import { GraphQLDeprecatedDirective, SingleFieldSubscriptionsRule } from "graphql";
-import React,  {useContext} from "react";
+import React, { useContext } from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
 import { FETCH_POSTS_QUERY } from "../util/graphql";
 import { Card, Grid, Loader } from "semantic-ui-react";
-import moment from 'moment';
-import LikeButton from '../components/LikeButton';
+import moment from "moment";
+import LikeButton from "../components/LikeButton";
 
-import { AuthContext } from '../context/auth';
+import { AuthContext } from "../context/auth";
 
 function SinglePost(props) {
   const postId = props.match.params.postid;
+  const { user } = useContext(AuthContext);
   console.log(postId);
   const {
     data: { getPost },
@@ -42,35 +42,46 @@ function SinglePost(props) {
     } = getPost;
 
     postMarkup = (
-        <Grid>
-          <Grid.Row>
-               <Grid.Column width={2}>
-               <Image 
-               src="https://react.semantic-ui.com/images/avatar/large/molly.png"
-               size="small"
-               float="right"/>
-               </Grid.Column>
-               <Grid.Column width={10}>
-               <Card fluid>
-                   <Card.Content>
-                       <Card.Header>{username}</Card.Header>
-                       <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
-                       <Card.Description>{body}</Card.Description>
-                   </Card.Content>
-                   <hr/>
-                   <Card.Content extra>
-                       <LikeButton></LikeButton>
-                   </Card.Content>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image
+              src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+              size="small"
+              float="right"
+            />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+              </Card.Content>
+              <hr />
+              <Card.Content extra>
+                <LikeButton user={user} post={{ id, likeCount, likes }}/>
+                <Button 
+                as="div"
+                labelPosition="right"
+                onClick={() => console.log('Comment on post')}
+                >
+                    <Button basic color='blue'>
+                        <Icon name="comments"/>
+                    </Button>
+                    <Label basic color="blue" pointing="left">
+                        {commentCount}
+                    </Label>
+                </Button>
 
-               </Card>
-               </Grid.Column>
-          </Grid.Row>
-        </Grid>
-    )
-    }
-
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
   }
-
+}
 
 const FETCH_POST_QUERY = gql`
   query($postId: ID!) {
