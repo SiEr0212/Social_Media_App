8bb5c457394f24dddfd72ad45b7fd0b01bbb35e0
SiEr0212 <simonertel@gmx.de>
2021-02-13 00:04:03 +0100

    added new component SinglePost

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,9 +1,10 @@
-import { SingleFieldSubscriptionsRule } from "graphql";
+import { GraphQLDeprecatedDirective, SingleFieldSubscriptionsRule } from "graphql";
 import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
 import { FETCH_POSTS_QUERY } from "../util/graphql";
-import { Loader } from "semantic-ui-react";
+import { Card, Grid, Loader } from "semantic-ui-react";
+import moment from 'moment';
 
 function SinglePost(props) {
   const postId = props.match.params.postid;
@@ -37,10 +38,26 @@ function SinglePost(props) {
       commentCount,
     } = getPost;
 
-    postMarkup = {
+    postMarkup = (
         <Grid>
-        
+          <Grid.Row>
+               <Grid.Column width={2}>
+               <Image 
+               src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+               size="small"
+               float="right"/>
+               </Grid.Column>
+               <Grid.Column width={10}>
+               <Card fluid>
+                   <Card.Content>
+                       <Card.Header>{username}</Card.Header>
+                       <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                   </Card.Content>
+               </Card>
+               </Grid.Column>
+          </Grid.Row>
         </Grid>
+    )
     }
 
   }
