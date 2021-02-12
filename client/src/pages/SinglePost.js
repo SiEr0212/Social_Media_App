15b111c42327d33233ea372ce5d0845745383607
SiEr0212 <simonertel@gmx.de>
2021-02-12 23:41:29 +0100

    added new component SinglePost

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,8 +1,31 @@
-import { SingleFieldSubscriptionsRule } from 'graphql'
-import React from 'react'
+import { SingleFieldSubscriptionsRule } from "graphql";
+import React from "react";
+import gql from "graphql-tag";
 
-function SinglePost(props){
-
+function SinglePost(props) {
+  const postId = props.match.params.postid;
 }
+
+const FETCH_POST_QUERY = gql`
+  query($postId: ID!) {
+    getPosts(postId: $postId) {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default SinglePost;
