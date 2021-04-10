dc3510dbeaf4eee4ad4a330aba6ec0c27162950e
SiEr0212 <simonertel@gmx.de>
2021-04-11 01:27:49 +0200

    added new server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+const port = process.env.PORT || 3001
+
+app.post('/api/upload', (req, res) => {
+    
+
+})
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+
+})
