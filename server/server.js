08cfbe52220e56473bfce536d4f515e06be23e02
SiEr0212 <simonertel@gmx.de>
2021-04-11 19:04:31 +0200

    adding serrver configs for cloudinary api

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-const port = process.env.PORT || 3001
-
-app.post('/api/upload', (req, res) => {
-    
-
-})
+app.post("/api/upload", (req, res) => {
+  try {
+    const fileStr = req.body.data;
+    console.log(fileStr);
+  } catch (error) {
+    console.error(error);
+  }
+});
+const port = 5000;  //process.env.PORT || 3001
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-
-})
+  console.log(`listening on port ${port}`);
+});
