1de6889db55e8b1504b6504efb105845ef5bed31
SiEr0212 <simonertel@gmx.de>
2021-04-15 21:10:33 +0200

    changing upload.js to firebase upload

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,105 +1,15 @@
-import React, { useState } from 'react';
-import Alert from '../components/Alert';
-import axios from 'axios';
+import React from 'react';
 
 export default function Upload() {
-    const [fileInputState, setFileInputState] = useState('');
-    const [previewSource, setPreviewSource] = useState('');
-    const [selectedFile, setSelectedFile] = useState();
-    const [successMsg, setSuccessMsg] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const handleFileInputChange = (e) => {
-        const file = e.target.files[0];
-        previewFile(file);
-        setSelectedFile(file);
-        setFileInputState(e.target.value);
-    };
-
-    //new tutorial
-const [selectFile, setselectFile] = useState(null)
-
-
-
-
-
-    //end new tutorial
-
-    const previewFile = (file) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = () => {
-            setPreviewSource(reader.result);
-        };
-    };
-
-    const handleSubmitFile = (e) => {
-        e.preventDefault();
-        if (!selectedFile) return;
-        const reader = new FileReader();
-        reader.readAsDataURL(selectedFile);
-        reader.onloadend = () => {
-            uploadImage(reader.result);
-        };
-        reader.onerror = () => {
-            console.error('AHHHHHHHH!!');
-            setErrMsg('something went wrong!');
-        };
-    };
-
-    const uploadImage = async (base64EncodedImage) => {
-        try {
-            await fetch('/api/upload', {
-                method: 'POST',
-                body: JSON.stringify({ data: base64EncodedImage }),
-                headers: { 'Content-Type': 'application/json' },
-            });
-            setFileInputState('');
-            setPreviewSource('');
-            setSuccessMsg('Image uploaded successfully');
-        } catch (err) {
-            console.error(err);
-            setErrMsg('Something went wrong!');
-        }
-    };
-
-    const fileSelectedHandler = (e) => {
-        console.log(e.target.files[0])
-        setselectFile(e.target.file[0])
+    const onChangeHandler = () => {
 
     }
-
-    const fileUploadHandler = () => {
-        axios.post('cloudinary://137344693198534:T64FoeyRYYz00Kce1uNpwmMy1lg@simonscloud2021');
-    }
-   
     return (
-        <div>
-            <h1>Other tutorial</h1>
-            <input type="file" onChange={fileSelectedHandler}/>
-            <button onClick={fileUploadHandler}>Upload</button>
-            <h1 className="title">Upload an Image</h1>
-            <Alert msg={errMsg} type="danger" />
-            <Alert msg={successMsg} type="success" />
-            <form onSubmit={handleSubmitFile} className="form">
-                <input
-                    id="fileInput"
-                    type="file"
-                    name="image"
-                    onChange={handleFileInputChange}
-                    value={fileInputState}
-                    className="form-input"
-                />
-                <button className="btn" type="submit">
-                    Submit
-                </button>
-            </form>
-            {previewSource && (
-                <img
-                    src={previewSource}
-                    alt="chosen"
-                    style={{ height: '300px' }}
-                />
-            )}
-        </div>
-    );
+        <>
+        <form>
+        <input type="file" onChange={onChangeHandler}/>
+        <input type="text" name="user"/>
+        </form>
+        </>
+    )
 }
