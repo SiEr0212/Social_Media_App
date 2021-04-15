170e12c04aeebbf4cb52f041ef384cb2fe40bbae
SiEr0212 <simonertel@gmx.de>
2021-04-15 21:26:44 +0200

    firebase upload connection working

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React from "react";
+import { app } from "../base";
 
 export default function Upload() {
-    const onChangeHandler = () => {
+  const onFileChange = (e) => {
+    const file = e.target.files[0];
+    const storageRef = app.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    fileRef.put(file).then(() => {
+      console.log("Uploaded file", file.name);
+    });
+  };
 
-    }
-    return (
-        <>
-        <form>
-        <input type="file" onChange={onChangeHandler}/>
-        <input type="text" name="user"/>
-        </form>
-        </>
-    )
+  const onSubmitHandler = (e) => {
+    e.preventDefault(); // so when you click submit it doesen't triggger navigation
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmitHandler}>
+        <input type="file" onChange={onFileChange} />
+        <input type="text" name="user" placeholder="NAME" />
+        <button>Submit</button>
+      </form>
+      <ul>
+        <li>---</li>
+      </ul>
+    </>
+  );
 }
