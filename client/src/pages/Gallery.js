52dc9fd40071b4dc244c1270a488dcc62987df15
SiEr0212 <simonertel@gmx.de>
2021-04-20 23:51:26 +0200

    adding imgPath to Userschema

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,8 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { app } from "../base";
 import { AuthContext } from "../context/auth";
-import { Button, Form } from 'semantic-ui-react';
-
+import { Button, Form } from "semantic-ui-react";
 
 const db = app.firestore();
 
@@ -10,7 +9,6 @@ export default function Gallery() {
   const { user } = useContext(AuthContext);
   const [fileUrl, setFileUrl] = React.useState(null);
   const [users, setUsers] = React.useState([]); // users from firebase
- 
 
   const onFileChange = async (e) => {
     const file = e.target.files[0];
@@ -46,22 +44,22 @@ export default function Gallery() {
   return (
     <>
       <Form onSubmit={onSubmit}>
-      <Form.Field>
-        <Form.Input type="file" onChange={onFileChange} />
-         <Form.Input type="text" name="username" placeholder="NAME" />
-        <Button  color="teal">Submit</Button>
-      </Form.Field>
+        <Form.Field>
+          <Form.Input type="file" onChange={onFileChange} />
+          <Form.Input type="text" name="username" placeholder="NAME" />
+          <Button color="teal">Submit</Button>
+        </Form.Field>
       </Form>
       <ul>
         {users.map((useR) => {
           return (
             <>
-           {/*<li key={useR.name}>*/} 
+              {/*<li key={useR.name}>*/}
               <img width="100" height="100" src={useR.avatar} alt={useR.name} />
               {/*<p>{useR.name}</p>*/}
-            {/*</li>*/}
-          </>
-            );
+              {/*</li>*/}
+            </>
+          );
         })}
       </ul>
     </>
